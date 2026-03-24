b02fb418e253363b8833dc0aeb9c597a2c7a663a
Shubham Mahalle <shubhammahallee@gmail.com>
2026-03-24 18:43:26 +0530

    Implement anagram check functionality

--- Strings/anagram_check.py
+++ Strings/anagram_check.py
@@ -1,1 +1,10 @@
+s1 = input("enter first word: ")
+s2 = input("enter second word: ")
 
+s1_sorted = sorted(s1)
+s2_sorted = sorted(s2)
+
+if s1_sorted == s2_sorted:
+    print("Anagram")
+else:
+    print("Not Anagram")
