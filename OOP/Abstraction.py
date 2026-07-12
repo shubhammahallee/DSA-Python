a7cf16ce9dc193dcd35177ed55285887dbaf366e
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-12 08:42:48 +0530

    Update Abstraction.py

--- OOP/Abstraction.py
+++ OOP/Abstraction.py
@@ -10,6 +10,32 @@ class Vehicle(ABC):
     @abstractmethod
     def stop(self):
         pass
+        
+# Child Class 1
+class Car(Vehicle):
+
+    def start(self):
+        print("Car Started")
+
+    def stop(self):
+        print("Car Stopped")
+
+
+# Child Class 2
+class Bike(Vehicle):
+
+    def start(self):
+        print("Bike Started")
+
+    def stop(self):
+        print("Bike Stopped")
+
+
+# Driver Code
+car = Car()
+car.start()
+car.stop()
+
 
 
 
