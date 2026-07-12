8dc73c65363d1fe9a8c769ea7428b41d85785679
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-12 08:42:20 +0530

    Update Abstraction.py

--- OOP/Abstraction.py
+++ OOP/Abstraction.py
@@ -1,1 +1,19 @@
+from abc import ABC, abstractmethod
 
+# Abstract Class
+class Vehicle(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+
+
+bike = Bike()
+bike.start()
+bike.stop()
